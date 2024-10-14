ccb1ea5f2b208d40f8eb8efd335ea5adba4df55c
wmmeg <wmmegal@gmail.com>
2024-10-14 15:50:09 +0300

    feat: contacts and modal

--- src/js/common.js
+++ src/js/common.js
@@ -12,10 +12,10 @@ Alpine.data('app', () => ({
     rule: true,
     showMenu: true,
     matchMedia() {
-        return window.matchMedia('(min-width: 1200px)').matches;
+        this.showMenu = window.matchMedia('(min-width: 1200px)').matches;
     },
     init() {
-        this.showMenu = this.matchMedia();
+        this.matchMedia();
     },
 }));
 
@@ -36,6 +36,24 @@ if (splideCta) {
             },
         },
     }).mount();
+}
+
+const splideProduct = document.querySelector('.splide-product');
+
+if (splideProduct) {
+    const splide = new Splide('.splide-product', {
+        type: 'loop',
+        arrows: false,
+        pagination: false,
+        autoplay: true,
+        interval: 5000,
+    }).mount();
+
+    const btnPrev = document.querySelector('.js-btn-prev'),
+        btnNext = document.querySelector('.js-btn-next');
+
+    btnPrev.addEventListener('click', () => splide.go('<'));
+    btnNext.addEventListener('click', () => splide.go('>'));
 }
 
 Fancybox.bind();
